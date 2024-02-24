783fa8897150a6588a1a621e92a49ce0c673ecb7
lingfliu <lingfeng.liu@163.com>
2024-02-24 23:42:54 +0800

    added stub code

--- main_infer.py
+++ main_infer.py
@@ -1,10 +1,39 @@
 """inference framework"""
 import time
 import cv2
+import vid_source
 
 # definining data source
 vid_url = './resources/sample.S01E08.HD1080p.mp4'
-import vid_source
+# configurations
+resize = (300, 300)
+
+buff_size = 100
+frame_buffed = []
+detect_buffed = []
+
+def component_detect(frame_buffed, frame_resized):
+    # TODO implement the detection algorithm
+    return  [
+        {"class": "lhand", "confidence": 0.9, "bbox": [0, 0, 100, 100], "id": 1},
+        {"class": "rhand", "confidence": 0.9, "bbox": [0, 0, 100, 100], "id": 1},
+    ]
+
+# should return the adjacency matrix of skeleton
+def connect_component(detect):
+    # TODO implement the connection algorithm
+    return {}
+
+def st_traj_matching(skeletons):
+    return [(1,2), (2,3)]
+
+def recog_single(motion):
+    return 5, 0.9
+
+def recog_hoi(motion):
+    return 5, 0.9
+
+
 
 if __name__ == '__main__':
     '''inference from video source'''
@@ -28,12 +57,47 @@ if __name__ == '__main__':
         # sleep 0.02 seconds
         toc = time.time()
         # get the time difference
-        dts.append(toc-tic)
-        if len(dts) > 1000:
-            print("fps: ", 1/(sum(dts)/len(dts)))
-            dts = []
-        print("time: ", toc-tic)
-        tic = toc
+        # dts.append(toc-tic)
+        # if len(dts) > 1000:
+        #     print("fps: ", 1/(sum(dts)/len(dts)))
+        #     dts = []
+        # print("time: ", toc-tic)
+        # tic = toc
+
+        # preprocessing
+        frame_resized = cv2.resize(frame, resize)
+
+        detect = component_detect(frame_buffed, frame_resized)
+        frame_buffed.append(frame_resized)
+        if len(frame_buffed) > buff_size:
+            frame_buffed.pop(0)
+
+        detect_buffed.append(detect)
+        if len(detect_buffed) > buff_size:
+            detect_buffed.pop(0)
+
+        skeletons = connect_component(detect)
+
+        matched = st_traj_matching(skeletons)
+
+        # TODO generate human-object motion sequences
+        motion = []
+
+        for match in matched:
+            skeleton,  obj = match
+            if obj is None:
+                # no interaction
+                recog_single(motion)
+            elif not obj.type == 'skeleton':
+                # object to object interaction
+                recog_hoi(motion)
+            else: # skeleton to skeleton interaction
+                recog_hoi(motion)
+
+        # TODO draw results on frames
+
+        # TODO save the results to a video file
+
     vid_source.close_vid(cap)
 
 
